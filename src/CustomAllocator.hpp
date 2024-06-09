862fe2c8dbcb24ebe5f58f5de45eb10f4e62643c
rudikrudik <rudik.login@gmail.com>
2024-06-09 20:47:25 +0300

    add dinamic allocate memory

--- src/CustomAllocator.hpp
+++ src/CustomAllocator.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
+#include <array>
+
 template<class T, signed S>
 class CustomPoolAllocator {
 public:
     typedef T value_type;
-    static signed int position;
     static constexpr int size = sizeof(T) * S;
-    void* data = ::operator new(size);
 
-    CustomPoolAllocator() noexcept = default;
+    CustomPoolAllocator() : memory_block(0) {};
     ~CustomPoolAllocator() = default;
 
-    template<class TA, signed SA>
-    constexpr explicit CustomPoolAllocator(const CustomPoolAllocator<TA, SA> &other) noexcept;
+    template<class TA>
+    constexpr explicit CustomPoolAllocator(const CustomPoolAllocator<TA, S> &other) noexcept;
 
     T* allocate (std::size_t n);
     void deallocate (T* p, std::size_t n);
@@ -21,43 +21,45 @@ public:
     struct rebind {using other = CustomPoolAllocator<TA, S>;};
 
     // Перегрузка операторов
-    template<class TA, signed UA>
-    constexpr bool operator == (const CustomPoolAllocator<TA, UA>& a) noexcept;
-    template<class TA, signed UA>
-    constexpr bool operator != (const CustomPoolAllocator<TA, UA>& a) noexcept;
+    template<class TA>
+    constexpr bool operator == (const CustomPoolAllocator<TA, S>& a) noexcept;
+    template<class TA>
+    constexpr bool operator != (const CustomPoolAllocator<TA, S>& a) noexcept;
+
+private:
+    std::array<value_type, S> memory;
+    size_t memory_block = 0;
 
 };
 
 // Реализация методов
 template<class T, signed S>
-template<class TA, signed SA>
-constexpr CustomPoolAllocator<T,S>::CustomPoolAllocator(const CustomPoolAllocator<TA, SA> &other) noexcept {}
+template<class TA>
+constexpr CustomPoolAllocator<T,S>::CustomPoolAllocator(const CustomPoolAllocator<TA, S> &other) noexcept {}
 
 template<class T, signed S>
 T* CustomPoolAllocator<T, S>::allocate(std::size_t n) {
-    std::cout << "Allocated elem" << std::endl;
-    if (sizeof(T)*position > size)
+    if (memory_block + n > S)
         throw std::bad_alloc();
 
-    int current = position;
-    position += static_cast<int>(n);
-    return reinterpret_cast<T*>(data) + current;
+    T* current_pointer = &memory[memory_block];
+    memory_block += n;
+    return current_pointer;
 }
 
 template<class T, signed S>
-[[maybe_unused]] void CustomPoolAllocator<T, S>::deallocate(T*, std::size_t){}
+void CustomPoolAllocator<T, S>::deallocate(T*, std::size_t n){
+    memory_block -= n;
+}
 
 template<class T, signed int S>
-template<class TA, signed int UA>
-constexpr bool CustomPoolAllocator<T, S>::operator==(const CustomPoolAllocator<TA, UA> &a) noexcept {
+template<class TA>
+constexpr bool CustomPoolAllocator<T, S>::operator==(const CustomPoolAllocator<TA, S> &a) noexcept {
     return true;
 }
 
 template<class T, signed int S>
-template<class TA, signed int UA>
-constexpr bool CustomPoolAllocator<T, S>::operator!=(const CustomPoolAllocator<TA, UA> &a) noexcept {
+template<class TA>
+constexpr bool CustomPoolAllocator<T, S>::operator!=(const CustomPoolAllocator<TA, S> &a) noexcept {
     return false;
 }
-
-template <class T, signed int S>
-int CustomPoolAllocator<T, S>::position = 0;
